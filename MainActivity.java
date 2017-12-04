99e7e210a2de43c1786de7e9f272549ad3c7ce9c
EvZhdan <ev107@mail.ru>
2017-12-04 20:50:10 +0300

    Commit

--- MainActivity.java
+++ MainActivity.java
@@ -1,42 +1,68 @@
 package ru.drivermql.p0016;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutCompat;
 import android.view.Gravity;
-import android.view.ViewGroup;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
-import android.widget.TextView;
+import android.widget.RadioGroup;
 
-import static android.view.Gravity.CENTER_HORIZONTAL;
+public class MainActivity extends Activity implements View.OnClickListener {
 
-public class MainActivity extends AppCompatActivity {
+    LinearLayout llMain;
+    RadioGroup rbGravity;
+    EditText etName;
+    Button btnCreate;
+    Button dtnClear;
 
+    int wrapContent = LinearLayout.LayoutParams.WRAP_CONTENT;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //создание LinearLayout
-        LinearLayout linearLayout = new LinearLayout(this);
-        //установим вертикальную ориентацию
-        linearLayout.setOrientation(LinearLayout.VERTICAL);
-        //Создаем LayoutParams
-        LinearLayoutCompat.LayoutParams layoutParams = new LinearLayoutCompat.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+        setContentView(R.layout.activity_main);
+        //находим элементы
+        llMain = (LinearLayout) findViewById(R.id.llMain);
+        rbGravity = (RadioGroup) findViewById(R.id.rbGravity);
+        etName = (EditText) findViewById(R.id.etName);
 
-        //устанавливаем linearLayout как корневой элемент экрана
-        setContentView(linearLayout,layoutParams);
+        btnCreate = (Button) findViewById(R.id.btnCreate);
+        btnCreate.setOnClickListener(this);
 
-        LinearLayoutCompat.LayoutParams ipView = new LinearLayoutCompat.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-        ipView.gravity= CENTER_HORIZONTAL;
-        TextView textView = new TextView(this);
-        textView.setText("TextView");
-        textView.setLayoutParams(ipView);
-        linearLayout.addView(textView);
+        dtnClear = (Button) findViewById(R.id.btnClear);
+        dtnClear.setOnClickListener(this);
+    }
 
-
-
-        Button button = new Button(this);
-        button.setText("Button");
-        linearLayout.addView(button,ipView);
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.btnCreate:
+                //создание LayoutParams с шириной и высотой по содержимому
+                LinearLayout.LayoutParams iParams = new LinearLayout.LayoutParams(wrapContent,wrapContent);
+        //переменная для хранения значения выравнивания
+        //по умолчанию будет LEFT
+                int btnGravity = Gravity.LEFT;
+                //определяем, какой RadioButton чекнут и заполняем
+                //btnGravity
+                switch (rbGravity.getCheckedRadioButtonId()){
+                    case R.id.rbLeft:
+                        btnGravity = Gravity.LEFT;
+                        break;
+                    case R.id.rbCenter:
+                        btnGravity = Gravity.CENTER_HORIZONTAL;
+                        break;
+                    case R.id.rbRight:
+                        btnGravity = Gravity.RIGHT;
+                        break;
+                }
+                //переносим полученное значение выравнивания в LinearLayout
+                iParams.gravity = btnGravity;
+                //создаем Button, пишем текст и добавляем в LinearLayout
+                Button btnNew = new Button(this);
+                btnNew.setText(etName.getText().toString());
+                llMain.addView(btnNew,iParams);
+                break;
+        }
     }
 }
